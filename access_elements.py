aaf4b2e289c73c5e54e6d72e9365f11dd5d1dcf9
Jospine <nzi19001@byui.edu>
2024-04-09 13:06:14 +0300

    sequence

---
+++ access_elements.py
@@ -0,0 +1,17 @@
+#access elements by using python
+list=[1,2,3,4,5,6]
+print(list[0])
+#output 1
+#2 slice
+list_2 =[30,4,5,6,7,8]
+print(list_2[1:5])
+#concatenate
+concatenate =list+list_2
+print(concatenate)
+#repeat list 3 times
+repeat = list*3
+print(repeat)
+#pick one element from the list 
+print(3 in list)#output =true
+#length
+print(len(list))
