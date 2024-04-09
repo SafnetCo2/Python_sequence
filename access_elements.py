99f92eef4b1412fa2264dcd2f743b76ffba025b9
Jospine <nzi19001@byui.edu>
2024-04-09 13:34:03 +0300

    range

--- access_elements.py
+++ access_elements.py
@@ -15,3 +15,19 @@ print(repeat)
 print(3 in list)#output =true
 #length
 print(len(list))
+list_3=['cabbage','apples','onions']
+list_3.sort()
+print(list_3)
+months=['jan','feb','march']
+months.sort()
+print(months)
+sortkey=['this is my laptop','word','two']
+sortkey.sort(key=len, reverse=True)
+print(sortkey)
+#add to list
+sortkey[0]='cabbage'
+sortkey.append('kiwi')
+print(sortkey)
+
+for x in range(4):
+    print(x)
