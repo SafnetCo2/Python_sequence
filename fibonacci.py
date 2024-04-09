ebaa7a54a214256c16d151eccf108140031d3c16
Jospine <nzi19001@byui.edu>
2024-04-10 00:53:45 +0300

    done

--- fibonacci.py
+++ fibonacci.py
@@ -7,4 +7,20 @@ def print_fibonacci(length):
     for num in fibonacci_sequence:
         print(num, end=" ")
 print_fibonacci(10)
+
+class Employee:
+    def __init__(self, name, emp_id,salary):
+        self.name=name
+        self.emp_id=emp_id
+        self.salary=salary
+    def salary_update(self,new_salary):
+        self.salary = new_salary
+    def get_details(self):
+        return f"The new employee {self.name} identification {self.emp_id} earns {self.salary}"
+emp=Employee("John ","Eml001", 60000)
+print(emp.get_details())
+emp.salary_update(65000)
+print(emp.get_details())
+        
+
     
